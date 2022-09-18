08288dfd215ecb6f65e2c409958e3ffdde591a4f
bri25yu <bri25yu@gmail.com>
2022-09-18 14:40:39 -0700

    removing prefix tuning decoder prefix; implementing prefix tuning encoder prefix

--- attention_driven/modeling/prefix_tuning.py
+++ attention_driven/modeling/prefix_tuning.py
@@ -97,10 +97,34 @@ def prefix_tuning_encoder_forward(
     embed_pos = self.embed_positions(input_ids, inputs_embeds)
 
     hidden_states = inputs_embeds + embed_pos
+
+    ###################################
+    # START prefix tuning hidden states
+    ###################################
+
+    hidden_states = torch.concat((self.prefix, hidden_states))
+
+    ###################################
+    # END prefix tuning hidden states
+    ###################################
+
     hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
 
     # expand attention_mask
     if attention_mask is not None:
+
+        ###################################
+        # START prefix tuning attention mask
+        ###################################
+
+        L = self.prefix.size()[0]
+        prefix_attention_mask = torch.ones(L, device=attention_mask.device)
+        attention_mask = torch.concat((prefix_attention_mask, attention_mask))
+
+        ###################################
+        # END prefix tuning attention mask
+        ###################################
+
         # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
         attention_mask = _expand_mask(attention_mask, inputs_embeds.dtype)
 
@@ -159,240 +183,6 @@ def prefix_tuning_encoder_forward(
     )
 
 
-# This is an exact copy of the M2M100Decoder forward function except denoted otherwise
-def prefix_tuning_decoder_forward(
-    self,
-    input_ids=None,
-    attention_mask=None,
-    encoder_hidden_states=None,
-    encoder_attention_mask=None,
-    head_mask=None,
-    cross_attn_head_mask=None,
-    past_key_values=None,
-    inputs_embeds=None,
-    use_cache=None,
-    output_attentions=None,
-    output_hidden_states=None,
-    return_dict=None,
-):
-    r"""
-    Args:
-        input_ids (`torch.LongTensor` of shape `(batch_size, sequence_length)`):
-            Indices of input sequence tokens in the vocabulary. Padding will be ignored by default should you
-            provide it.
-
-            Indices can be obtained using [`M2M100Tokenizer`]. See
-            [`PreTrainedTokenizer.encode`] and [`PreTrainedTokenizer.__call__`]
-            for details.
-
-            [What are input IDs?](../glossary#input-ids)
-        attention_mask (`torch.Tensor` of shape `(batch_size, sequence_length)`, *optional*):
-            Mask to avoid performing attention on padding token indices. Mask values selected in `[0, 1]`:
-
-            - 1 for tokens that are **not masked**,
-            - 0 for tokens that are **masked**.
-
-            [What are attention masks?](../glossary#attention-mask)
-        encoder_hidden_states (`torch.FloatTensor` of shape `(batch_size, encoder_sequence_length, hidden_size)`, *optional*):
-            Sequence of hidden-states at the output of the last layer of the encoder. Used in the cross-attention
-            of the decoder.
-        encoder_attention_mask (`torch.LongTensor` of shape `(batch_size, encoder_sequence_length)`, *optional*):
-            Mask to avoid performing cross-attention on padding tokens indices of encoder input_ids. Mask values
-            selected in `[0, 1]`:
-
-            - 1 for tokens that are **not masked**,
-            - 0 for tokens that are **masked**.
-
-            [What are attention masks?](../glossary#attention-mask)
-        head_mask (`torch.Tensor` of shape `(decoder_layers, decoder_attention_heads)`, *optional*):
-            Mask to nullify selected heads of the attention modules. Mask values selected in `[0, 1]`:
-
-            - 1 indicates the head is **not masked**,
-            - 0 indicates the head is **masked**.
-
-        cross_attn_head_mask (`torch.Tensor` of shape `(decoder_layers, decoder_attention_heads)`, *optional*):
-            Mask to nullify selected heads of the cross-attention modules in the decoder to avoid performing
-            cross-attention on hidden heads. Mask values selected in `[0, 1]`:
-
-            - 1 indicates the head is **not masked**,
-            - 0 indicates the head is **masked**.
-
-        past_key_values (`tuple(tuple(torch.FloatTensor))`, *optional*, returned when `use_cache=True` is passed or when `config.use_cache=True`):
-            Tuple of `tuple(torch.FloatTensor)` of length `config.n_layers`, with each tuple having 2
-            tensors of shape `(batch_size, num_heads, sequence_length, embed_size_per_head)`) and 2 additional
-            tensors of shape `(batch_size, num_heads, encoder_sequence_length, embed_size_per_head)`.
-
-            Contains pre-computed hidden-states (key and values in the self-attention blocks and in the
-            cross-attention blocks) that can be used (see `past_key_values` input) to speed up sequential
-            decoding.
-
-            If `past_key_values` are used, the user can optionally input only the last
-            `decoder_input_ids` (those that don't have their past key value states given to this model) of
-            shape `(batch_size, 1)` instead of all ``decoder_input_ids``` of shape `(batch_size,
-            sequence_length)`. inputs_embeds (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`, *optional*): Optionally, instead of passing `input_ids` you can choose to directly pass an embedded representation. This is useful if you want more control over how to convert `input_ids` indices
-            into associated vectors than the model's internal embedding lookup matrix.
-        output_attentions (`bool`, *optional*):
-            Whether or not to return the attentions tensors of all attention layers. See `attentions` under
-            returned tensors for more detail.
-        output_hidden_states (`bool`, *optional*):
-            Whether or not to return the hidden states of all layers. See `hidden_states` under returned tensors
-            for more detail.
-        return_dict (`bool`, *optional*):
-            Whether or not to return a [`~file_utils.ModelOutput`] instead of a plain tuple.
-    """
-    output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-    output_hidden_states = (
-        output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
-    )
-    use_cache = use_cache if use_cache is not None else self.config.use_cache
-    return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-    # retrieve input_ids and inputs_embeds
-    if input_ids is not None and inputs_embeds is not None:
-        raise ValueError("You cannot specify both decoder_input_ids and decoder_inputs_embeds at the same time")
-    elif input_ids is not None:
-        input_shape = input_ids.size()
-        input_ids = input_ids.view(-1, input_shape[-1])
-    elif inputs_embeds is not None:
-        input_shape = inputs_embeds.size()[:-1]
-    else:
-        raise ValueError("You have to specify either decoder_input_ids or decoder_inputs_embeds")
-
-    # past_key_values_length
-    past_key_values_length = past_key_values[0][0].shape[2] if past_key_values is not None else 0
-
-    if inputs_embeds is None:
-        inputs_embeds = self.embed_tokens(input_ids) * self.embed_scale
-
-    # create causal mask
-    # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-    combined_attention_mask = None
-    if input_shape[-1] > 1:
-        combined_attention_mask = _make_causal_mask(
-            input_shape, inputs_embeds.dtype, past_key_values_length=past_key_values_length
-        ).to(self.device)
-
-    if attention_mask is not None and combined_attention_mask is not None:
-        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-        combined_attention_mask = combined_attention_mask + _expand_mask(
-            attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1]
-        )
-
-    # expand encoder attention mask
-    if encoder_hidden_states is not None and encoder_attention_mask is not None:
-        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-        encoder_attention_mask = _expand_mask(encoder_attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1])
-
-    # embed positions
-    positions = self.embed_positions(input_ids, inputs_embeds, past_key_values_length)
-
-    hidden_states = inputs_embeds + positions
-
-    hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
-
-    # decoder layers
-    all_hidden_states = () if output_hidden_states else None
-    all_self_attns = () if output_attentions else None
-    all_cross_attentions = () if output_attentions else None
-    next_decoder_cache = () if use_cache else None
-
-    # check if head_mask/cross_attn_head_mask has a correct number of layers specified if desired
-    for attn_mask, mask_name in zip([head_mask, cross_attn_head_mask], ["head_mask", "cross_attn_head_mask"]):
-        if attn_mask is not None:
-            assert attn_mask.size()[0] == (
-                len(self.layers)
-            ), f"The `{mask_name}` should be specified for {len(self.layers)} layers, but it is for {head_mask.size()[0]}."
-    for idx, decoder_layer in enumerate(self.layers):
-        # add LayerDrop (see https://arxiv.org/abs/1909.11556 for description)
-        if output_hidden_states:
-            all_hidden_states += (hidden_states,)
-        dropout_probability = random.uniform(0, 1)
-        if self.training and (dropout_probability < self.layerdrop):
-            continue
-
-        past_key_value = past_key_values[idx] if past_key_values is not None else None
-
-        if self.gradient_checkpointing and self.training:
-
-            if use_cache:
-                #######################
-                # START no logging
-                #######################
-
-                # Original code
-                # logger.warning(
-                #     "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
-                # )
-
-                #######################
-                # END no logging
-                #######################
-
-                use_cache = False
-
-            def create_custom_forward(module):
-                def custom_forward(*inputs):
-                    # None for past_key_value
-                    return module(*inputs, output_attentions, use_cache)
-
-                return custom_forward
-
-            layer_outputs = torch.utils.checkpoint.checkpoint(
-                create_custom_forward(decoder_layer),
-                hidden_states,
-                combined_attention_mask,
-                encoder_hidden_states,
-                encoder_attention_mask,
-                head_mask[idx] if head_mask is not None else None,
-                cross_attn_head_mask[idx] if cross_attn_head_mask is not None else None,
-                None,
-            )
-        else:
-
-            layer_outputs = decoder_layer(
-                hidden_states,
-                attention_mask=combined_attention_mask,
-                encoder_hidden_states=encoder_hidden_states,
-                encoder_attention_mask=encoder_attention_mask,
-                layer_head_mask=(head_mask[idx] if head_mask is not None else None),
-                cross_attn_layer_head_mask=(
-                    cross_attn_head_mask[idx] if cross_attn_head_mask is not None else None
-                ),
-                past_key_value=past_key_value,
-                output_attentions=output_attentions,
-                use_cache=use_cache,
-            )
-        hidden_states = layer_outputs[0]
-
-        if use_cache:
-            next_decoder_cache += (layer_outputs[3 if output_attentions else 1],)
-
-        if output_attentions:
-            all_self_attns += (layer_outputs[1],)
-            all_cross_attentions += (layer_outputs[2],)
-
-    hidden_states = self.layer_norm(hidden_states)
-
-    # add hidden states from the last decoder layer
-    if output_hidden_states:
-        all_hidden_states += (hidden_states,)
-
-    next_cache = next_decoder_cache if use_cache else None
-    if not return_dict:
-        return tuple(
-            v
-            for v in [hidden_states, next_cache, all_hidden_states, all_self_attns, all_cross_attentions]
-            if v is not None
-        )
-    return BaseModelOutputWithPastAndCrossAttentions(
-        last_hidden_state=hidden_states,
-        past_key_values=next_cache,
-        hidden_states=all_hidden_states,
-        attentions=all_self_attns,
-        cross_attentions=all_cross_attentions,
-    )
-
-
 def create_prefix_parameter(L: int, D: int, std: float) -> nn.Module:
     prefix = Parameter(torch.empty((L, D)))
 
@@ -413,9 +203,10 @@ class PrefixTuningM2M100ForConditionalGeneration(M2M100ForConditionalGeneration)
         for param in self.parameters():
             param.requires_grad = False
 
-        # Retrieve our encoder and decoder
+        # Retrieve our encoder
+        # The original prefix tuning paper prepends a prefix to both the
+        # encoder and decoder, but we start with encoder only for now
         encoder = self.model.encoder
-        decoder = self.model.decoder
 
         # Add prefix params
         L = self.prefix_length
@@ -423,10 +214,7 @@ class PrefixTuningM2M100ForConditionalGeneration(M2M100ForConditionalGeneration)
         std = self.config.init_std
 
         encoder.prefix = create_prefix_parameter(L, D, std)
-        decoder.prefix = create_prefix_parameter(L, D, std)
 
-        # Modify the forward function of the encoder and decoders
+        # Modify the forward function of the encoder
         # See https://stackoverflow.com/questions/972/adding-a-method-to-an-existing-object-instance
-
         encoder.forward = MethodType(prefix_tuning_encoder_forward, encoder)
-        decoder.forward = MethodType(prefix_tuning_decoder_forward, decoder)
