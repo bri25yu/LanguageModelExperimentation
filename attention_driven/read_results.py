b4cf6b6cff15b15c4acc2e1376e00046f75f204e
bri25yu <bri25yu@gmail.com>
2022-09-22 20:07:57 -0700

    improving read results

--- attention_driven/read_results.py
+++ attention_driven/read_results.py
@@ -1,10 +1,15 @@
+from typing import Any, Dict, List
+
 import os
 import pickle
+
+import pandas as pd
 
 from attention_driven import RESULTS_DIR
 
 
 def main():
+    data: List[Dict[str, Any]] = []
     for experiment_name in os.listdir(RESULTS_DIR):
         if not experiment_name.endswith("Experiment"):
             continue
@@ -13,13 +18,46 @@ def main():
         with open(results_path, "rb") as f:
             results = pickle.load(f)
 
-        print(experiment_name)
         for learning_rate, result_by_lr in sorted(results.items()):
-            print(f"  {learning_rate:.0e}")
             for split_name, result in result_by_lr.items():
                 loss = result.metrics["test_loss"]
                 score = result.metrics["test_score"]
-                print(f"    {split_name.ljust(10)} | loss {loss:.2f} score {score:.1f}")
+
+                config_data = {
+                    "name": experiment_name,
+                    "lr": learning_rate,
+                    "split": split_name,
+                    "loss": loss,
+                    "score": score,
+                }
+
+                data.append(config_data)
+
+    # Create a dataframe and clean it up some
+    df = pd.DataFrame(data)
+
+    # Generate display order
+    df = df.sort_values(["name", "lr"])
+
+    # Some display improvements
+    df.lr = df.lr.map(lambda lr: f"{lr:.0e}")
+    df.loss = df.loss.round(3)
+    df.score = df.score.round(1)
+    df.name = df.name.str.removesuffix("Experiment")
+
+    # Create a spreadsheet
+    df = df.pivot_table(
+        index=["name", "lr"],
+        columns="split",
+        values=["loss", "score"],
+        sort=False,
+    )
+
+    # Order the columns properly
+    columns_rordered = pd.MultiIndex.from_product([["loss", "score"], ["train", "val", "test"]], names=df.columns.names)
+    df = pd.DataFrame(df, columns=columns_rordered)
+
+    print(df)
 
 
 if __name__ == "__main__":
