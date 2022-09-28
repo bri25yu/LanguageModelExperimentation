a5085523cf0a3ba009e2f4dfba50a78ffad1423f
bri25yu <bri25yu@gmail.com>
2022-09-27 18:58:36 -0700

    dynamically importing experiments and adjusting run.py

--- attention_driven/experiments/__init__.py
+++ attention_driven/experiments/__init__.py
@@ -1,19 +1,68 @@
-from attention_driven.experiments.baseline import BaselineExperiment
-from attention_driven.experiments.attention_driven_dropout import (
-    AttentionDrivenConfig1Experiment,
-    AttentionDrivenConfig2Experiment,
-    AttentionDrivenConfig3Experiment,
-)
-from attention_driven.experiments.lora import (
-    LoRAConfig1Experiment,
-    LoRAConfig2Experiment,
-    LoRAConfig3Experiment,
-    LoRAConfig4Experiment,
-    LoRAConfig5Experiment,
-)
-from attention_driven.experiments.prefix_tuning import (
-    PrefixTuningNaiveConfig1Experiment,
-    PrefixTuningNaiveConfig2Experiment,
-    PrefixTuningNaiveConfig3Experiment,
-    PrefixTuningNaiveConfig4Experiment,
+from typing import Dict, Type
+
+import sys
+from types import ModuleType
+
+from importlib import import_module
+import pkgutil
+
+
+__all__ = ["available_experiments"]
+
+
+def get_experiments_from_module(module: ModuleType) -> Dict[str, Type]:
+    experiments: Dict[str, Type] = dict()
+    for attr_name in dir(module):
+        ends_with_experiment = attr_name.endswith("Experiment")
+        if not ends_with_experiment:
+            continue
+
+        value = getattr(module, attr_name)
+        is_type = isinstance(value, type)
+        if not is_type:
+            continue
+
+        experiment_cls = value
+        experiments[experiment_cls.__name__] = experiment_cls
+
+    return experiments
+
+
+def recursively_discover_experiments(
+    package: ModuleType,
+) -> Dict[str, Type]:
+    """
+    Recursively import all submodules of the input module, and returns the
+    classes which:
+    1. Name ends in "Experiment" e.g. "BaselineExperiment"
+
+    Parameters
+    ----------
+    package: ModuleType
+        The input package to discover experiments in.
+    Returns
+    -------
+    experiments: Dict[str, Type]
+        A mapping of experiment name to experiment class.
+    """
+    package_name = package.__name__
+
+    experiments = {}
+    for _, child_package_name, is_package in pkgutil.walk_packages(package.__path__):
+        child_package_full_name = f"{package_name}.{child_package_name}"
+        child_module = import_module(child_package_full_name)
+
+        # If the current module is a package, we need to recurse into its submodules
+        if is_package:
+            experiments.update(recursively_discover_experiments(child_module))
+
+        # We update our experiments dict with experiments from this module
+        experiments.update(get_experiments_from_module(child_module))
+
+    return experiments
+
+
+current_module = sys.modules[__name__]
+available_experiments = recursively_discover_experiments(
+    current_module
 )
--- run.py
+++ run.py
@@ -4,16 +4,26 @@ This is a convenience script for running experiments
 import shutil
 
 from attention_driven import TRAIN_OUTPUT_DIR
-from attention_driven.experiments import *
+from attention_driven.experiments import available_experiments
 
 
-EXPERIMENTS_TO_RUN = [  # Each element is a tuple (experiment class, batch size, learning rates: Optional[List[float]])
+EXPERIMENTS_TO_RUN = [  # Each element is a tuple (experiment name, batch size, learning rates: Optional[List[float]])
 ]
 
 
-for experiment_cls, batch_size, learning_rates in EXPERIMENTS_TO_RUN:
+for experiment_name, batch_size, learning_rates in EXPERIMENTS_TO_RUN:
+    experiment_cls = available_experiments.get(experiment_name, None)
+
+    if experiment_cls is None:
+        print(f"Experiment {experiment_name} is not recognized")
+        continue
+
     for learning_rate in learning_rates:
         experiment = experiment_cls()
-        experiment.run(batch_size, [learning_rate])
+
+        try:
+            experiment.run(batch_size, [learning_rate])
+        except Exception as e:
+            print(e)
 
         shutil.rmtree(TRAIN_OUTPUT_DIR, ignore_errors=True)
