10b7a7fc21e173fb54b273af1c1deacfa438c1ed
bri25yu <bri25yu@gmail.com>
2022-10-01 16:06:18 -0700

    adding custom mt5 data processing

--- attention_driven/experiments/finetune_mt5.py
+++ attention_driven/experiments/finetune_mt5.py
@@ -2,11 +2,15 @@ from collections import OrderedDict
 
 import torch
 
+from datasets import DatasetDict
+
 from transformers.tokenization_utils import PreTrainedTokenizer
 from transformers.modeling_utils import PreTrainedModel
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoConfig, MT5ForConditionalGeneration
 
 from attention_driven.experiments.baseline_v2 import BaselineV2Experiment
+from attention_driven.data_processors import LDTibetanEnglishDataV2Processor
+from attention_driven.data_processors.utils import convert_df_to_hf_dataset
 
 
 __all__ = ["FinetuneMT5BaseExperiment", "FinetuneMT5LargeExperiment", "FinetuneMT5XLExperiment"]
@@ -62,6 +66,43 @@ class FinetuneMT5ExperimentBase(BaselineV2Experiment):
         model.config.max_length = max_input_length
 
         return model
+
+    # This is the exact same function as BaselineV2Experiment unless noted otherwise
+    def load_data(self, tokenizer: PreTrainedTokenizer) -> DatasetDict:
+        """
+        This function assumes that https://github.com/Linguae-Dharmae/language-models
+        has been cloned into the same root folder.
+        """
+        val_split_size = self.VAL_SPLIT_SIZE
+        max_input_length = self.MAX_INPUT_LENGTH
+
+        # Load our datasets from disk into HF Dataset's
+        data_processor = LDTibetanEnglishDataV2Processor()
+
+        train_dataset, test_dataset = convert_df_to_hf_dataset(data_processor())
+        train_val_dataset = train_dataset.train_test_split(val_split_size, seed=42)
+
+        dataset = DatasetDict(
+            train=train_val_dataset["train"],
+            val=train_val_dataset["test"],
+            test=test_dataset,
+        )
+        print("Human readable dataset:", dataset)
+
+        def tokenize_fn(examples):
+            prefix = "translate to english: "
+            tibetan_inputs = [prefix + t for t in examples["tibetan"]]
+            model_inputs = tokenizer(tibetan_inputs, max_length=max_input_length, truncation=True)
+
+            labels = tokenizer(text_target=examples["english"], max_length=max_input_length, truncation=True)
+
+            model_inputs["labels"] = labels["input_ids"]
+            return model_inputs
+
+        tokenized_dataset = dataset.map(tokenize_fn, batched=True, remove_columns=["tibetan", "english"])
+        print("Model readable dataset:", tokenized_dataset)
+
+        return tokenized_dataset
 
 
 class FinetuneMT5BaseExperiment(FinetuneMT5ExperimentBase):
