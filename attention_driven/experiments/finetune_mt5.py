6cbaa69f909eabbdd60c401c3e8b724e781ea719
bri25yu <bri25yu@gmail.com>
2022-10-01 17:14:20 -0700

    separating mt5 exps with and without custom tokenizer; adding fp16 capable mt5 from https://github.com/huggingface/transformers/issues/14189#issuecomment-961571628

--- attention_driven/experiments/finetune_mt5.py
+++ attention_driven/experiments/finetune_mt5.py
@@ -11,25 +11,18 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoConfig, MT5Fo
 from attention_driven.experiments.baseline_v2 import BaselineV2Experiment
 from attention_driven.data_processors import LDTibetanEnglishDataV2Processor
 from attention_driven.data_processors.utils import convert_df_to_hf_dataset
+from attention_driven.modeling.mt5_fp16_utils import scale_weights_for_fp16_t5
+from attention_driven.modeling.mt5_fp16 import MT5Fp16ForConditionalGeneration
 
 
 __all__ = ["FinetuneMT5BaseExperiment", "FinetuneMT5LargeExperiment", "FinetuneMT5XLExperiment"]
 
 
+# We use a special version fp16 capable version of MT5
 class FinetuneMT5ExperimentBase(BaselineV2Experiment):
     MODEL_NAME = None
 
     def get_tokenizer(self) -> PreTrainedTokenizer:
-        """
-        We don't train the tokenizer on Tibetan corpora at the moment, but this is probably something we want to do.
-
-        https://github.com/huggingface/transformers/tree/main/examples/flax/language-modeling#t5-like-span-masked-language-modeling
-        """
-
-        """
-        tokenizer = AutoTokenizer.from_pretrained("buddhist-nlp/mt5-tibetan-tokenizer")
-        """
-
         model_name = self.MODEL_NAME
 
         tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -40,34 +33,15 @@ class FinetuneMT5ExperimentBase(BaselineV2Experiment):
         model_name = self.MODEL_NAME
         max_input_length = self.MAX_INPUT_LENGTH
 
-        """
-        # Load pretrained parameter weights
-        base_model_parameter_dict = AutoModelForSeq2SeqLM.from_pretrained(model_name).state_dict()
-        base_model_parameter_dict = OrderedDict(base_model_parameter_dict)  # Make `base_model_parameter_dict` modifiable
+        # We don't have access to bf16 capable Ampere + GPUs so we need to workaround it
+        model = MT5Fp16ForConditionalGeneration.from_pretrained(model_name)
+        scale_weights_for_fp16_t5(model)
 
-        keys_to_modify = ["shared.weight", "encoder.embed_tokens.weight", "decoder.embed_tokens.weight", "lm_head.weight"]
-        pretrained_embedding_weights = {k: base_model_parameter_dict.pop(k) for k in keys_to_modify}
-
-        # Create new model
-        config = AutoConfig.from_pretrained(model_name, vocab_size=tokenizer.vocab_size + 2)
-        model = MT5ForConditionalGeneration(config)
-
-        # Load pretrained weights into new model with a slight change to embeddings
-        # since we have a larger vocab size
-        model.load_state_dict(base_model_parameter_dict, strict=False)
-        model_parameter_dict = model.state_dict()
-        with torch.no_grad():
-            for weight_name, pretrained_embedding_weight in pretrained_embedding_weights.items():
-                pretrained_vocab_size, hidden_dim = pretrained_embedding_weight.size()
-                model_parameter_dict[weight_name][:pretrained_vocab_size, :hidden_dim].copy_(pretrained_embedding_weight)
-        """
-
-        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
         model.config.max_length = max_input_length
 
         return model
 
-    # This is the exact same function as BaselineV2Experiment unless noted otherwise
+    # This is the exact same function as `BaselineV2Experiment.load_data` unless noted otherwise
     def load_data(self, tokenizer: PreTrainedTokenizer) -> DatasetDict:
         """
         This function assumes that https://github.com/Linguae-Dharmae/language-models
@@ -90,11 +64,36 @@ class FinetuneMT5ExperimentBase(BaselineV2Experiment):
         print("Human readable dataset:", dataset)
 
         def tokenize_fn(examples):
+
+            ###########################
+            # START add mt5 prefix
+            ###########################
+
+            # Original code
+            # model_inputs = tokenizer(examples["tibetan"], max_length=max_input_length, truncation=True)
+
             prefix = "translate to english: "
             tibetan_inputs = [prefix + t for t in examples["tibetan"]]
             model_inputs = tokenizer(tibetan_inputs, max_length=max_input_length, truncation=True)
 
+            ###########################
+            # END add mt5 prefix
+            ###########################
+
+            ###########################
+            # START use text_target rather than tokenizer target context
+            ###########################
+
+            # Original code
+            # Set up the tokenizer for targets
+            # with tokenizer.as_target_tokenizer():
+            #     labels = tokenizer(examples["english"], max_length=max_input_length, truncation=True)
+
             labels = tokenizer(text_target=examples["english"], max_length=max_input_length, truncation=True)
+
+            ###########################
+            # END use text_target rather than tokenizer target context
+            ###########################
 
             model_inputs["labels"] = labels["input_ids"]
             return model_inputs
@@ -103,6 +102,45 @@ class FinetuneMT5ExperimentBase(BaselineV2Experiment):
         print("Model readable dataset:", tokenized_dataset)
 
         return tokenized_dataset
+
+
+# We add a custom Tibetan tokenizer in v2
+class FinetuneMT5V2ExperimentBase(FinetuneMT5ExperimentBase):
+    def get_tokenizer(self) -> PreTrainedTokenizer:
+        tokenizer = AutoTokenizer.from_pretrained("buddhist-nlp/mt5-tibetan-tokenizer")
+
+        return tokenizer
+
+    def get_model(self, tokenizer: PreTrainedTokenizer) -> PreTrainedModel:
+        model_name = self.MODEL_NAME
+        max_input_length = self.MAX_INPUT_LENGTH
+
+        # Load pretrained parameter weights
+        base_model_parameter_dict = AutoModelForSeq2SeqLM.from_pretrained(model_name).state_dict()
+        base_model_parameter_dict = OrderedDict(base_model_parameter_dict)  # Make `base_model_parameter_dict` modifiable
+
+        keys_to_modify = ["shared.weight", "encoder.embed_tokens.weight", "decoder.embed_tokens.weight", "lm_head.weight"]
+        pretrained_embedding_weights = {k: base_model_parameter_dict.pop(k) for k in keys_to_modify}
+
+        # Create new model
+        config = AutoConfig.from_pretrained(model_name, vocab_size=tokenizer.vocab_size + 2)
+        model = MT5Fp16ForConditionalGeneration(config)
+
+        # Load pretrained weights into new model with a slight change to embeddings
+        # since we have a larger vocab size
+        model.load_state_dict(base_model_parameter_dict, strict=False)
+        model_parameter_dict = model.state_dict()
+        with torch.no_grad():
+            for weight_name, pretrained_embedding_weight in pretrained_embedding_weights.items():
+                pretrained_vocab_size, hidden_dim = pretrained_embedding_weight.size()
+                model_parameter_dict[weight_name][:pretrained_vocab_size, :hidden_dim].copy_(pretrained_embedding_weight)
+
+        # We don't have access to bf16 capable Ampere + GPUs so we need to workaround it
+        scale_weights_for_fp16_t5(model)
+
+        model.config.max_length = max_input_length
+
+        return model
 
 
 class FinetuneMT5BaseExperiment(FinetuneMT5ExperimentBase):
@@ -114,4 +152,16 @@ class FinetuneMT5LargeExperiment(FinetuneMT5ExperimentBase):
 
 
 class FinetuneMT5XLExperiment(FinetuneMT5ExperimentBase):
+    MODEL_NAME = "google/mt5-xl"
+
+
+class FinetuneMT5BaseV2Experiment(FinetuneMT5V2ExperimentBase):
+    MODEL_NAME = "google/mt5-base"
+
+
+class FinetuneMT5LargeV2Experiment(FinetuneMT5V2ExperimentBase):
+    MODEL_NAME = "google/mt5-large"
+
+
+class FinetuneMT5XLV2Experiment(FinetuneMT5V2ExperimentBase):
     MODEL_NAME = "google/mt5-xl"
---
+++ attention_driven/modeling/mt5_fp16_utils.py
@@ -0,0 +1,55 @@
+"""
+Adapted from https://github.com/huggingface/transformers/issues/14189#issuecomment-961571628
+"""
+from typing import Union
+
+import torch
+
+from transformers import T5PreTrainedModel
+
+
+__all__ = ["scale_weights_for_fp16_t5"]
+
+
+# The same architectures in the model share the same weight scaling
+# The embeddings scaling is the maximum scaling performed
+ARCH_SCALING = {
+    "embeddings": 1 / 32.0,
+    "attention_value": 1 / 4.0,
+    "attention_output": 1 / 8.0,
+    "feedforward_weights_in": 1 / 4.0,
+    "feedforward_weights_out": 1 / 4.0,
+    "feedforward_layernorm": 1 / 2.0,
+}
+
+assert ARCH_SCALING["attention_value"] * ARCH_SCALING["attention_output"] == ARCH_SCALING["embeddings"]
+assert ARCH_SCALING["feedforward_weights_in"] * ARCH_SCALING["feedforward_weights_out"] * ARCH_SCALING["feedforward_layernorm"] == ARCH_SCALING["embeddings"]
+
+
+WEIGHT_SCALING = {
+    "shared.weight": ARCH_SCALING["embeddings"],
+    "SelfAttention.v": ARCH_SCALING["attention_value"],
+    "SelfAttention.o": ARCH_SCALING["attention_output"],
+    "EncDecAttention.v": ARCH_SCALING["attention_value"],
+    "EncDecAttention.o": ARCH_SCALING["attention_output"],
+    "lm_scale_modifier": 1 / ARCH_SCALING["embeddings"]
+}
+
+
+def scale_weights_for_fp16_t5(model: T5PreTrainedModel) -> None:
+    assert hasattr(model, "lm_scale_modifier"), "This function is only to be used with a modified mt5 model"
+
+    def search_for_scaling(weight_name: str) -> Union[None, float]:
+        for weight_name_infix in WEIGHT_SCALING:
+            if weight_name_infix in weight_name:
+                return WEIGHT_SCALING[weight_name_infix]
+
+        return None
+
+    with torch.no_grad():
+        for weight_name, weight in model.state_dict():
+            scaling = search_for_scaling(weight_name)
+            if scaling is None:
+                continue
+
+            weight *= scaling
