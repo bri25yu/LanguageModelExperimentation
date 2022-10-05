a666340f82c5f05a3cdec6e5dfafa1ffbc7c92b6
bri25yu <bri25yu@gmail.com>
2022-10-04 17:12:40 -0700

    updating output test translations

--- attention_driven/output_test_translations.py
+++ attention_driven/output_test_translations.py
@@ -1,24 +1,24 @@
-import os
+from argparse import ArgumentParser
 
 import pickle
 
-import pandas as pd
-
-import evaluate
-
-from transformers import AutoTokenizer
-
-from attention_driven import RESULTS_DIR
-from attention_driven.data_processors import LDTibetanEnglishDataProcessor
+from attention_driven.experiments import available_experiments
+from attention_driven.data_processors import LDTibetanEnglishDataV2Processor
 
 
-EXPERIMENT_NAME = "BaselineExperiment"
+def load_experiment():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--experiment", "-e", required=True, help="Experiment name to print predictions for"
+    )
+
+    args = parser.parse_args()
+
+    return available_experiments[args.experiment]()
 
 
-def load_results():
-    results_path = os.path.join(RESULTS_DIR, EXPERIMENT_NAME, "predictions")
-
-    with open(results_path, "rb") as f:
+def load_results(experiment):
+    with open(experiment.predictions_output_path, "rb") as f:
         results = pickle.load(f)
 
     best_results, best_val_score = None, float("-inf")
@@ -26,42 +26,27 @@ def load_results():
         val_result = result_by_lr["val"]
         test_result = result_by_lr["test"]
 
-        val_score = val_result.metrics["test_score"]
+        val_score = val_result.metrics["test_bleu_score"]
         if val_score > best_val_score:
             best_results, best_val_score = test_result, val_score
 
     return best_results
 
 
-def decode_tokens(results):
-    tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M")
+def decode_tokens(experiment, results):
+    tokenizer = experiment.get_tokenizer()
 
     predictions = results.predictions
-    label_ids = results.label_ids
-
     predictions = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-    references = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
 
-    return predictions, references
-
-
-def get_bleu_score(predictions, references):
-    bleu = evaluate.load("bleu")
-
-    metrics = bleu.compute(predictions=predictions, references=references)
-
-    return metrics["bleu"] * 100
+    return predictions
 
 
 def main():
-    print("Loading dataset")
-    _, df = LDTibetanEnglishDataProcessor()()
-
-    print("Loading results")
-    results = load_results()
-
-    print("Decoding output tokens")
-    predictions, references = decode_tokens(results)
+    experiment = load_experiment()
+    _, df = LDTibetanEnglishDataV2Processor()()
+    results = load_results(experiment)
+    predictions = decode_tokens(experiment, results)
 
     for i in range(5):
         print(f"*****Example translation {i+1}*****")
@@ -70,14 +55,9 @@ def main():
         print("Predicted translation:", predictions[i])
         print()
 
-    print("Calculating BLEU score")
-    bleu_score = get_bleu_score(predictions, references)
-
-    print(f"{EXPERIMENT_NAME} achieves a BLEU score of {bleu_score:.1f}")
-
     df["predictions"] = predictions
 
-    df.to_csv(f"{EXPERIMENT_NAME}_predictions.tsv", sep="\t", index=False)
+    df.to_csv(f"{experiment.__class__.__name__}_predictions.tsv", sep="\t", index=False)
 
 
 if __name__ == "__main__":
