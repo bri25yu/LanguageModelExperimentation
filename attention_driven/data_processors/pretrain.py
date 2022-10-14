99b34ab68b1e6ec7a9f37580a78e4edaf9c27585
bri25yu <bri25yu@gmail.com>
2022-10-14 15:27:31 -0700

    adding unimplemented pretrain data processor

---
+++ attention_driven/data_processors/pretrain.py
@@ -0,0 +1,30 @@
+from datasets import load_dataset, DatasetDict
+
+from attention_driven.data_processors.abstract import AbstractDataProcessor
+
+
+class PretrainDataProcessor(AbstractDataProcessor):
+    """
+    The original loaded dataset dict is 
+    DatasetDict({
+        tibetan: Dataset({
+            features: ['text'],
+            num_rows: 10156565
+        })
+        english: Dataset({
+            features: ['text'],
+            num_rows: 2034185
+        })
+        chinese: Dataset({
+            features: ['text'],
+            num_rows: 26165641
+        })
+    })
+    """
+
+    def load(self) -> DatasetDict:
+        # !TODO
+        raise NotImplementedError
+        dataset = load_dataset("buddhist-nlp/pretraining_data", use_auth_token=True)
+
+        return dataset
