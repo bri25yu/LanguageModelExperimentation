1e0046354016dbb727491c5cff986de61d9ea42b
bri25yu <bri25yu@gmail.com>
2022-10-14 20:48:39 -0700

    adding mt5 and nllb model mixins

---
+++ attention_driven/experiments/model_mixins/mt5_model_mixins.py
@@ -0,0 +1,42 @@
+from typing import Union
+
+from attention_driven.experiments.experiment_base import ExperimentBase
+
+from transformers.tokenization_utils import PreTrainedTokenizer
+from transformers.modeling_utils import PreTrainedModel
+
+from transformers import AutoTokenizer
+
+from attention_driven.modeling.mt5_fp16_utils import scale_weights_for_fp16_t5
+from attention_driven.modeling.mt5_fp16 import MT5Fp16ForConditionalGeneration
+
+
+class MT5ModelMixinBase(ExperimentBase):
+    MODEL_NAME: Union[None, str] = None
+
+    def get_tokenizer(self) -> PreTrainedTokenizer:
+        model_name = self.MODEL_NAME
+
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+        return tokenizer
+
+    def get_model(self, tokenizer: PreTrainedTokenizer) -> PreTrainedModel:
+        model_name = self.MODEL_NAME
+        max_input_length = self.MAX_INPUT_LENGTH
+
+        # We don't have access to bf16 capable Ampere + GPUs so we need to workaround it
+        model = MT5Fp16ForConditionalGeneration.from_pretrained(model_name)
+        scale_weights_for_fp16_t5(model)
+
+        model.config.max_length = max_input_length
+
+        return model
+
+
+class MT5Base580MModelMixin(MT5ModelMixinBase):
+    MODEL_NAME = "google/mt5-base"
+
+
+class MT5Large1_2BModelMixin(MT5ModelMixinBase):
+    MODEL_NAME = "google/mt5-large"
---
+++ attention_driven/experiments/model_mixins/nllb_model_mixins.py
@@ -0,0 +1,45 @@
+from typing import Union
+
+from attention_driven.experiments.experiment_base import ExperimentBase
+
+from transformers.tokenization_utils import PreTrainedTokenizer
+from transformers.modeling_utils import PreTrainedModel
+
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+
+class NLLBModelMixinBase(ExperimentBase):
+    MODEL_NAME: Union[None, str] = None
+
+    def get_tokenizer(self) -> PreTrainedTokenizer:
+        model_name = self.MODEL_NAME
+
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+        # We use a unidirectional tokenizer from Tibetan to English
+        tokenizer.src_lang = "bod_Tibt"
+        tokenizer.tgt_lang = "eng_Latn"
+
+        return tokenizer
+
+    def get_model(self, tokenizer: PreTrainedTokenizer) -> PreTrainedModel:
+        model_name = self.MODEL_NAME
+        max_input_length = self.MAX_INPUT_LENGTH
+
+        assert model_name, f"Must override `MODEL_NAME` attribute of {self.name}"
+
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+        model.config.max_length = max_input_length
+        model.config.forced_bos_token_id = tokenizer.lang_code_to_id["eng_Latn"]
+        model._keys_to_ignore_on_save = []
+
+        return model
+
+
+class NLLB600MModelMixin(NLLBModelMixinBase):
+    MODEL_NAME = "facebook/nllb-200-distilled-600M"
+
+
+class NLLB1_3BModelMixin(NLLBModelMixinBase):
+    MODEL_NAME = "facebook/nllb-200-1.3B"
