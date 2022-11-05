0389a607d0986a26d27d7223111ef1d775b7e868
bri25yu <bri25yu@gmail.com>
2022-11-05 16:26:32 -0700

    moving output folders into upper dir

--- attention_driven/__init__.py
+++ attention_driven/__init__.py
@@ -2,7 +2,7 @@ import os
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-RESULTS_DIR = os.path.join(ROOT_DIR, "results")
+RESULTS_DIR = os.path.join(ROOT_DIR, "..", "results")
 CONFIG_DIR = os.path.join(ROOT_DIR, "config")
-TRAIN_OUTPUT_DIR = os.path.join(ROOT_DIR, "train_output")
-DATASET_CACHE_DIR = os.path.join(ROOT_DIR, "dataset_cache")
+TRAIN_OUTPUT_DIR = os.path.join(ROOT_DIR, "..", "train_output")
+DATASET_CACHE_DIR = os.path.join(ROOT_DIR, "..", "dataset_cache")
