101faa9e389772ba40d4190ae1aab75e71d62af3
bri25yu <bri25yu@gmail.com>
2022-11-05 16:49:17 -0700

    removing redundant training arguments logic

--- lme/experiments/tib_to_eng_translation/tib_to_eng_translation_mixin.py
+++ lme/experiments/tib_to_eng_translation/tib_to_eng_translation_mixin.py
@@ -1,7 +1,5 @@
 from typing import Any, Callable, Dict
 
-import os
-
 from datasets import DatasetDict
 
 import evaluate
@@ -11,7 +9,6 @@ from transformers.tokenization_utils import PreTrainedTokenizer
 from transformers import (
     DataCollatorForSeq2Seq,
     TrainingArguments,
-    Seq2SeqTrainingArguments,
     Seq2SeqTrainer,
 )
 
@@ -27,13 +24,7 @@ class TibToEngTranslationMixin(ExperimentBase):
     Mixin for Tibetan to English translation finetuning
     """
     MAX_INPUT_LENGTH = 100
-    NUM_TRANSLATION_TRAIN_STEPS = 10000
-    TARGET_TOTAL_BATCH_SIZE_PER_UPDATE = 2 ** 8  # 256
-    NUM_TRANSLATION_EVAL_STEPS = 200
     FINETUNE_TRAINER_CLS = TRAINER_CLS = Seq2SeqTrainer
-
-    def get_training_arguments(self, batch_size: int, learning_rate: float) -> TrainingArguments:
-        return self.get_translation_training_arguments(batch_size, learning_rate)
 
     def get_data_collator(self, tokenizer: PreTrainedTokenizer) -> Callable:
         return self.get_translation_data_collator(tokenizer)
@@ -44,9 +35,6 @@ class TibToEngTranslationMixin(ExperimentBase):
     def get_tokenized_dataset(self, tokenizer: PreTrainedTokenizer, training_arguments: TrainingArguments) -> DatasetDict:
         return self.get_translation_dataset(tokenizer, training_arguments)
 
-    def get_finetune_training_arguments(self, batch_size: int, learning_rate: float) -> TrainingArguments:
-        return self.get_translation_training_arguments(batch_size, learning_rate)
-
     def get_finetune_data_collator(self, tokenizer: PreTrainedTokenizer) -> Callable:
         return self.get_translation_data_collator(tokenizer)
 
@@ -55,47 +43,6 @@ class TibToEngTranslationMixin(ExperimentBase):
 
     def get_finetune_dataset(self, tokenizer: PreTrainedTokenizer, finetune_training_arguments: TrainingArguments) -> DatasetDict:
         return self.get_translation_dataset(tokenizer, finetune_training_arguments)
-
-    def get_translation_training_arguments(self, batch_size: int, learning_rate: float) -> TrainingArguments:
-        output_dir = os.path.join(
-            self.experiment_class_output_dir, f"{learning_rate:.0e}"
-        )
-        max_steps = self.NUM_TRANSLATION_TRAIN_STEPS
-        eval_steps = self.NUM_TRANSLATION_EVAL_STEPS
-        target_total_batch_size_per_update = self.TARGET_TOTAL_BATCH_SIZE_PER_UPDATE
-        world_size = self.get_world_size()
-        per_gpu_batch_size = batch_size
-
-        eval_save_strategy = "steps"
-
-        gradient_accumulation_steps = target_total_batch_size_per_update // (per_gpu_batch_size * world_size)
-        gradient_accumulation_steps = max(gradient_accumulation_steps, 1)
-
-        return Seq2SeqTrainingArguments(
-            output_dir,
-            learning_rate=learning_rate,
-            load_best_model_at_end=True,
-            evaluation_strategy=eval_save_strategy,
-            save_strategy=eval_save_strategy,
-            max_steps=max_steps,
-            eval_steps=eval_steps,
-            save_steps=eval_steps,
-            save_total_limit=1,
-            per_device_train_batch_size=per_gpu_batch_size,
-            per_device_eval_batch_size=per_gpu_batch_size,
-            gradient_accumulation_steps=gradient_accumulation_steps,
-            eval_accumulation_steps=1,
-            do_train=True,
-            do_eval=True,
-            seed=42,
-            fp16=True,
-            log_level="error",
-            log_on_each_node=False,
-            logging_steps=1,
-            predict_with_generate=True,
-            warmup_steps=0,
-            deepspeed=self.load_deepspeed_template_args("WarmupLR"),
-        )
 
     def get_translation_dataset(self, tokenizer: PreTrainedTokenizer, training_arguments: TrainingArguments) -> DatasetDict:
         max_input_length = self.MAX_INPUT_LENGTH
--- lme/training_argument_mixins/pretrain.py
+++ lme/training_argument_mixins/pretrain.py
@@ -1,16 +1,34 @@
+from typing import Union
+
 from transformers import TrainingArguments
 
 from lme.training_argument_mixins.utils import get_seq2seq_training_arguments
+from lme.training_argument_mixins.finetune import (
+    ConstantLRFinetuneTrainingArgumentsMixin,
+)
 
 
-__all__ = ["PretrainTrainingArgumentsMixin"]
+__all__ = [
+    "PretrainTrainingArgumentsMixin1",
+]
 
 
-class PretrainTrainingArgumentsMixin:
-    def get_training_arguments(self, batch_size: int, learning_rate: float) -> TrainingArguments:
+class PretrainTrainingArgumentsMixinBase:
+    FINETUNE_TRAINING_ARGUMENTS_MIXIN: Union[None, type] = None
+
+    def get_finetune_training_arguments(self, batch_size: int, learning_rate: float) -> TrainingArguments:
+        return self.FINETUNE_TRAINING_ARGUMENTS_MIXIN.get_training_arguments(
+            self, batch_size, learning_rate
+        )
+
+
+class PretrainTrainingArgumentsMixin1(PretrainTrainingArgumentsMixinBase):
+    FINETUNE_TRAINING_ARGUMENTS_MIXIN = ConstantLRFinetuneTrainingArgumentsMixin
+
+    def get_pretrain_training_arguments(self, batch_size: int) -> TrainingArguments:
         return get_seq2seq_training_arguments(
             base_output_dir=self.experiment_class_output_dir,
-            learning_rate=learning_rate,
+            learning_rate=1e-4,
             max_steps=100000,
             eval_steps=1000,
             target_total_batch_size_per_update=2 ** 10,  # 1024
