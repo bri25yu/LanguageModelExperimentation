2e59b69486fb7bb96a6c5f575287ce6cb502ec39
bri25yu <bri25yu@gmail.com>
2022-11-07 17:18:47 -0800

    fixing mlm text grouping with prefix

--- lme/modeling/t5_span_mlm.py
+++ lme/modeling/t5_span_mlm.py
@@ -251,7 +251,7 @@ def get_group_texts_fn(
         mean_noise_span_length=mean_noise_span_length,
     )
 
-    def group_texts(examples) -> Dict[str, List[str]]:
+    def group_texts(examples: Dict[str, List[List[int]]]) -> Dict[str, List[int]]:
         # Concatenate all texts.
         concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
         total_length = len(concatenated_examples[list(examples.keys())[0]])
@@ -270,6 +270,56 @@ def get_group_texts_fn(
         return result
 
     return group_texts
+
+
+# This is an exact copy of `get_group_texts_fn` unless specified otherwise
+def get_group_texts_with_prefix_fn(
+    max_input_length: int,
+    tokenized_prefix: Dict[str, List[int]],
+    mlm_probability: float=DEFAULT_MLM_PROBABILITY,
+    mean_noise_span_length: float=DEFAULT_MEAN_NOISE_SPAN_LENGTH,
+) -> Callable:
+    expanded_inputs_length, _ = compute_input_and_target_lengths(
+        inputs_length=max_input_length,
+        noise_density=mlm_probability,
+        mean_noise_span_length=mean_noise_span_length,
+    )
+
+    def group_texts_with_prefix(examples: Dict[str, List[List[int]]]) -> Dict[str, List[int]]:
+        # Concatenate all texts.
+        concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+        total_length = len(concatenated_examples[list(examples.keys())[0]])
+
+        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+        # customize this part to your needs.
+        if total_length >= expanded_inputs_length:
+            total_length = (total_length // expanded_inputs_length) * expanded_inputs_length
+
+        ###############################
+        # START add prefix
+        ###############################
+
+        # Original code:
+        # Split by chunks of max_len.
+        # result = {
+        #     k: [t[i : i + expanded_inputs_length] for i in range(0, total_length, expanded_inputs_length)]
+        #     for k, t in concatenated_examples.items()
+        # }
+
+        prefix_length = len(tokenized_prefix[list(examples.keys())[0]])
+        length_with_prefix = expanded_inputs_length - prefix_length
+        result = {
+            k: [tokenized_prefix[k] + t[i : i + length_with_prefix] for i in range(0, total_length, length_with_prefix)]
+            for k, t in concatenated_examples.items()
+        }
+
+        ###############################
+        # END add prefix
+        ###############################
+
+        return result
+
+    return group_texts_with_prefix
 
 
 def create_t5_mlm_data_collator(
--- lme/training_dataset_utils/monolingual.py
+++ lme/training_dataset_utils/monolingual.py
@@ -2,41 +2,32 @@ from datasets import Dataset
 
 from transformers.tokenization_utils import PreTrainedTokenizerBase
 
-from lme.modeling.t5_span_mlm import get_group_texts_fn
+from lme.modeling.t5_span_mlm import get_group_texts_fn, get_group_texts_with_prefix_fn
 
 
 def tokenize_tibetan_monolingual(dataset: Dataset, max_input_length: int, tokenizer: PreTrainedTokenizerBase) -> Dataset:
-    def tokenize_fn(examples):
-        return tokenizer(examples["text"])
-
     group_texts = get_group_texts_fn(max_input_length)
 
-    return dataset \
-        .map(tokenize_fn, batched=True, remove_columns=["text"], desc="Tokenizing ") \
-        .map(group_texts, batched=True)
+    def tokenize_and_group_fn(examples):
+        return group_texts(tokenizer(examples["text"]))
+
+    return dataset.map(
+        tokenize_and_group_fn, batched=True, remove_columns=["text"], desc="Tokenizing and grouping monolingual tibetan"
+    )
 
 
-# This is an exact copy of `tokenize_tibetan_monolingual` unless specified otherwise
 def tokenize_tibetan_monolingual_with_prefix(
     dataset: Dataset, max_input_length: int, tokenizer: PreTrainedTokenizerBase, prefix: str
 ) -> Dataset:
-    def tokenize_fn(examples):
-        ###############################
-        # START add prefix
-        ###############################
+    tokenized_prefix = tokenizer(prefix)
+    group_texts_with_prefix = get_group_texts_with_prefix_fn(max_input_length, tokenized_prefix)
 
-        # Original code:
-        # return tokenizer(examples["text"])
+    def tokenize_and_group_fn(examples):
+        return group_texts_with_prefix(tokenizer(examples["text"]))
 
-        inputs_to_tokenize = [f"{prefix} {e}" for e in examples["text"]]
-        return tokenizer(inputs_to_tokenize)
-
-        ###############################
-        # END add prefix
-        ###############################
-
-    group_texts = get_group_texts_fn(max_input_length)
-
-    return dataset \
-        .map(tokenize_fn, batched=True, remove_columns=["text"], desc="Tokenizing ") \
-        .map(group_texts, batched=True)
+    return dataset.map(
+        tokenize_and_group_fn,
+        batched=True,
+        remove_columns=["text"],
+        desc=f"Tokenizing and grouping monolingual tibetan with prefix `{prefix}`",
+    )
