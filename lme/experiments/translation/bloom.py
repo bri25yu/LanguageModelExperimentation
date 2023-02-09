946243e88ec2b037c151ecb6c37be212d425a789
bri25yu <bri25yu@gmail.com>
2023-02-09 11:46:03 -0800

    reverting bloom data collator

--- lme/experiments/translation/bloom.py
+++ lme/experiments/translation/bloom.py
@@ -1,8 +1,3 @@
-from typing import Callable
-
-from transformers import default_data_collator
-from transformers.tokenization_utils import PreTrainedTokenizerBase
-
 from lme.training_pipelines import FinetuneExperimentBase
 from lme.model_mixins import *
 from lme.training_argument_mixins import BloomFinetuneArgsMixin
@@ -10,8 +5,7 @@ from lme.experiments.translation.mixin import TranslationMixin
 
 
 class TranslationBloomExperimentBase(BloomFinetuneArgsMixin, TranslationMixin, FinetuneExperimentBase):
-    def get_data_collator(self, tokenizer: PreTrainedTokenizerBase) -> Callable:
-        return default_data_collator
+    pass
 
 
 class TranslationBloom600MExperiment(Bloom600MModelMixin, TranslationBloomExperimentBase):
