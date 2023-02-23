ff28ce13dd45c2064b310ef855ef8435f82d64a4
bri25yu <bri25yu@gmail.com>
2023-02-22 21:51:01 -0800

    tydiqa no longer unnecessarily pads inputs

--- lme/training_dataset_utils/tydiqa.py
+++ lme/training_dataset_utils/tydiqa.py
@@ -5,7 +5,7 @@ from transformers.tokenization_utils import PreTrainedTokenizerBase
 
 def tokenize_tydiqa(dataset: DatasetDict, max_input_length: int, tokenizer: PreTrainedTokenizerBase) -> DatasetDict:
     def tokenize_fn(examples):
-        model_inputs = tokenizer(examples["context"], examples["question"], max_length=max_input_length, truncation="only_first", padding=True)
+        model_inputs = tokenizer(examples["context"], examples["question"], max_length=max_input_length, truncation="only_first")
 
         labels = tokenizer(text_target=examples["answer"])
 
