f01f6d68005fa6758dfdf09989058c46ba40c864
bri25yu <bri25yu@gmail.com>
2023-02-23 19:29:57 -0800

    increasing tydiqa batch size from 64 to 256

--- lme/training_argument_mixins/tydiqa.py
+++ lme/training_argument_mixins/tydiqa.py
@@ -14,14 +14,14 @@ __all__ = [
 
 class TyDiQAMT5FinetuneArgsMixin:
     """
-    This results in 10k * 64 = 640k examples for a train dataset size of ~50k.
+    This results in 10k * 256 = 2.5mil examples for a train dataset size of ~50k.
     """
     def get_training_arguments(self, batch_size: int, learning_rate: float) -> TrainingArguments:
         output_dir = os.path.join(
             self.experiment_class_output_dir, f"{learning_rate:.0e}"
         )
 
-        target_total_batch_size_per_update = 2 ** 6  # 64
+        target_total_batch_size_per_update = 2 ** 8  # 256
         gradient_accumulation_steps, per_device_batch_size = calculate_batch_size_args(target_total_batch_size_per_update, batch_size)
         return Seq2SeqTrainingArguments(
             output_dir=output_dir,
