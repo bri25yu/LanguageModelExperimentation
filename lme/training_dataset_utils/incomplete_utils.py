527cc7ed1ad4df6310fffb7510e737e763696933
hlillemark <hlillemark@gmail.com>
2023-02-23 22:19:11 -0800

    Fix prefix and suffix truncated func

--- lme/training_dataset_utils/incomplete_utils.py
+++ lme/training_dataset_utils/incomplete_utils.py
@@ -20,14 +20,22 @@ def add_prefix_truncated_output(inputs: Dict[str, Sequence], max_input_length: i
 def add_prefix_and_suffix_truncated_output(inputs: Dict[str, Sequence], max_input_length: int) -> None:
     # Truncate the labels and append it to the input, somewhere in the middle
     # [* * *] --------------- [* * * *]
+    # result_length = randint(len(inputs["labels"]), ())
+    # result_length = min(result_length, max_input_length - len(inputs["input_ids"]))
+
+    # Randomize the total length of the sequence based on the max length
+    # result_length = randint(max_input_length - len(inputs["input_ids"]), ())
     result_length = randint(len(inputs["labels"]), ())
     result_length = min(result_length, max_input_length - len(inputs["input_ids"]))
 
-    # Generate offset for starting position of the truncation
-    offset = randint(len(inputs["labels"]) - result_length, ())
+    # Generate suffix and prefix locations based on max length
+    prefix_cutoff = randint(result_length, ())
+    prefix = inputs["labels"][:prefix_cutoff]
+    suffix_cutoff = result_length - prefix_cutoff
+    suffix = inputs["labels"][-suffix_cutoff:]
 
-    # Add the prefix and suffix 
-    to_append = inputs["labels"][:offset] + inputs["labels"][(offset + result_length):]
+    # Add the prefix and suffix and a sentinel token in between
+    to_append = prefix + '<extra_id_12>' + suffix
 
     inputs["input_ids"] = inputs["input_ids"] + to_append
     inputs["attention_mask"] = inputs["attention_mask"] + [1] * len(to_append)
