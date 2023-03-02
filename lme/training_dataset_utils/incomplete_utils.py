19884cc47f0bd608d1bcc1ae2d0e191725681519
hlillemark <hlillemark@gmail.com>
2023-03-01 16:54:52 -0800

    Add incomplete experiment 8 and 10 with comments

--- lme/training_dataset_utils/incomplete_utils.py
+++ lme/training_dataset_utils/incomplete_utils.py
@@ -1,7 +1,7 @@
 
 from typing import Dict, Sequence
 from torch import randint
-
+from transformers.tokenization_utils import PreTrainedTokenizerBase
 
 
 def add_prefix_truncated_output(inputs: Dict[str, Sequence], max_input_length: int) -> None:
@@ -77,3 +77,35 @@ def add_suffix_truncated_output(inputs: Dict[str, Sequence], max_input_length: i
 
     inputs["input_ids"] = inputs["input_ids"] + to_append
     inputs["attention_mask"] = inputs["attention_mask"] + [1] * len(to_append)
+
+
+def add_masked_output(inputs: Dict[str, Sequence], max_input_length: int, tokenizer: PreTrainedTokenizerBase) -> None:
+    # Add masks randomly to the target output and append it to the training input
+    # [* * *] ---- [* * *] -- [* *] ------- [*]
+    
+    # Randomize number of masked tokens 
+    num_masks = randint(5, ()) + len(inputs["labels"]) // 5 + 1
+    last_cutoff = 0
+    next_cutoff = 0
+
+    for i in range(num_masks):
+        next_cutoff = next_cutoff + randint(9, ()) + 1
+        
+        if next_cutoff > len(inputs["labels"]):
+            break
+        addition = inputs["labels"][last_cutoff:next_cutoff]
+        inputs["input_ids"] = inputs["input_ids"] + addition + [tokenizer.pad_token_id]
+        next_cutoff = next_cutoff + randint(9, ()) + 1
+        last_cutoff = next_cutoff
+
+    if len(inputs["input_ids"]) >= max_input_length:
+        inputs["input_ids"] = inputs["input_ids"][:255] + [tokenizer.eos_token_id]
+    
+    # Strip extra eos or pad tokens 
+    while inputs["input_ids"][-1:] == [tokenizer.pad_token_id] or inputs["input_ids"][-1:] == [tokenizer.eos_token_id]:
+        inputs["input_ids"] = inputs["input_ids"][:-1]    
+    
+    inputs["input_ids"] = inputs["input_ids"] + [tokenizer.eos_token_id]
+
+    inputs["attention_mask"] = inputs["attention_mask"] + [1] * (len(inputs["input_ids"]) - len(inputs["attention_mask"]))
+
