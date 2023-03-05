097150a2641299b7b9b9e02942f6090092b8dd3f
bri25yu <bri25yu@gmail.com>
2023-03-04 20:06:31 -0800

    adding packed exp

--- lme/data_processors/flores200/__init__.py
+++ lme/data_processors/flores200/__init__.py
@@ -1,3 +1,4 @@
 from lme.data_processors.flores200.baseline_small import *
 from lme.data_processors.flores200.baseline_medium import *
 from lme.data_processors.flores200.incomplete import *
+from lme.data_processors.flores200.packed import *
