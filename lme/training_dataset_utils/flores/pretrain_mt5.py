81e730bdeb8304660842244b30a8fe90415f4474
bri25yu <bri25yu@gmail.com>
2023-03-17 22:53:22 -0700

    using correctly tokenized mt5 val test dict

--- lme/training_dataset_utils/flores/pretrain_mt5.py
+++ lme/training_dataset_utils/flores/pretrain_mt5.py
@@ -36,7 +36,7 @@ def main():
     corrupted_dataset = mask_and_create_labels_for_pretrain(tokenized_dataset, tokenizer, seed=SEED)
     print(f"Corrupted dataset\n{corrupted_dataset}\n{corrupted_dataset[0]}")
 
-    val_test_dict = load_dataset("bri25yu/flores200_val_test")
+    val_test_dict = load_dataset("bri25yu/flores200_baseline_medium_mt5")
     dataset_dict = DatasetDict({
         "train": corrupted_dataset,
         "val": val_test_dict["val"],
