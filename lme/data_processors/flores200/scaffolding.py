4f342446d7859428a35341b53bd4c14be40440de
hlillemark <hlillemark@gmail.com>
2023-03-23 23:53:23 -0700

    Add scaffolding experiments

---
+++ lme/data_processors/flores200/scaffolding.py
@@ -0,0 +1,50 @@
+from datasets import load_dataset, DatasetDict
+
+from lme.data_processors.abstract import AbstractDataProcessor
+
+
+__all__ = ["ScaffoldingOutputDataProcessor", "ScaffoldingInputDataProcessor"]
+
+
+class ScaffoldingInputDataProcessor(AbstractDataProcessor):
+    """
+    DatasetDict({
+        train: Dataset({
+            features: ['id', 'input_ids', 'attention_mask', 'labels'],
+            num_rows: 10240000
+        })
+        val: Dataset({
+            features: ['id', 'input_ids', 'attention_mask', 'labels'],
+            num_rows: 5000
+        })
+        test: Dataset({
+            features: ['id', 'input_ids', 'attention_mask', 'labels'],
+            num_rows: 10000
+        })
+    })
+    """
+
+    def load(self) -> DatasetDict:
+        return load_dataset("bri25yu/flores200_eng_input_scaffolding_mt5", use_auth_token=True)
+
+
+class ScaffoldingOutputDataProcessor(AbstractDataProcessor):
+    """
+    DatasetDict({
+        train: Dataset({
+            features: ['id', 'input_ids', 'attention_mask', 'labels'],
+            num_rows: 10240000
+        })
+        val: Dataset({
+            features: ['id', 'input_ids', 'attention_mask', 'labels'],
+            num_rows: 5000
+        })
+        test: Dataset({
+            features: ['id', 'input_ids', 'attention_mask', 'labels'],
+            num_rows: 10000
+        })
+    })
+    """
+
+    def load(self) -> DatasetDict:
+        return load_dataset("hlillemark/flores200_eng_output_scaffolding_mt5", use_auth_token=True)
