5e3b3cc5460f74a5c047d09afb747af1c62c7285
bri25yu <bri25yu@gmail.com>
2023-03-29 12:37:22 -0700

    adding baseline mt5 1b 20mil flores exp

---
+++ lme/experiments/flores_large_model/baseline_20mil.py
@@ -0,0 +1,32 @@
+from typing import Callable, List, Union
+
+from datasets import DatasetDict
+
+from transformers import DataCollatorForSeq2Seq, Seq2SeqTrainer, TrainingArguments
+from transformers.tokenization_utils import PreTrainedTokenizerBase
+
+from lme.compute_metrics_utils.flores200 import get_flores_compute_metrics
+
+from lme.data_processors.abstract import AbstractDataProcessor
+from lme.data_processors.flores200 import BaselineMediumDataProcessor
+
+from lme.model_mixins import MT51BModelMixin
+from lme.training_pipelines import FinetuneExperimentBase
+from lme.training_argument_mixins import FloresMT5FinetuneArgsMixin
+
+
+class FloresBaseline1B20milExperiment(MT51BModelMixin, FloresMT5FinetuneArgsMixin, FinetuneExperimentBase):
+    MAX_INPUT_LENGTH = 256
+    TRAINER_CLS = Seq2SeqTrainer
+    DATA_PROCESSOR_CLASSES: Union[None, List[AbstractDataProcessor]] = None
+
+    def get_data_collator(self, tokenizer: PreTrainedTokenizerBase) -> Callable:
+        max_input_length = self.MAX_INPUT_LENGTH
+
+        return DataCollatorForSeq2Seq(tokenizer, max_length=max_input_length, padding=True, pad_to_multiple_of=8)
+
+    def get_compute_metrics(self, tokenizer: PreTrainedTokenizerBase) -> Callable:
+        return get_flores_compute_metrics(tokenizer)
+
+    def get_tokenized_dataset(self, tokenizer: PreTrainedTokenizerBase, training_arguments: TrainingArguments) -> DatasetDict:
+        return BaselineMediumDataProcessor()(training_arguments)
