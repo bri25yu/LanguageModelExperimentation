17bbcc72d11e22aee7533b61dcbbbbb05db99866
hlillemark <hlillemark@gmail.com>
2023-04-01 01:16:24 -0700

    Make it like 600m

--- lme/experiments/flores_large_model/baseline_20mil.py
+++ lme/experiments/flores_large_model/baseline_20mil.py
@@ -4,35 +4,28 @@ from datasets import DatasetDict
 
 from transformers import DataCollatorForSeq2Seq, Seq2SeqTrainer, TrainingArguments
 from transformers.tokenization_utils import PreTrainedTokenizerBase
+from transformers.modeling_utils import PreTrainedModel
 
 from lme.compute_metrics_utils.flores200 import get_flores_compute_metrics
 
 from lme.data_processors.abstract import AbstractDataProcessor
 from lme.data_processors.flores200 import BaselineMediumDataProcessor
+from lme.experiments.flores_600m_exps.baseline import FloresStagedExperimentBase
 
 from lme.model_mixins import MT51BModelMixin
 from lme.training_pipelines import FinetuneExperimentBase
 from lme.training_argument_mixins import FloresMT5FinetuneArgsMixin
 
 
-class FloresBaseline1B20milExperiment(MT51BModelMixin, FloresMT5FinetuneArgsMixin, FinetuneExperimentBase):
-    MAX_INPUT_LENGTH = 256
-    TRAINER_CLS = Seq2SeqTrainer
-    DATA_PROCESSOR_CLASSES: Union[None, List[AbstractDataProcessor]] = None
+class FloresBaseline1BExperiment(MT51BModelMixin, FloresStagedExperimentBase):
     DATA_PROCESSOR_CLASSES = [BaselineMediumDataProcessor]
 
-    def get_data_collator(self, tokenizer: PreTrainedTokenizerBase) -> Callable:
-        max_input_length = self.MAX_INPUT_LENGTH
+    def update_training_arguments(self, training_arguments: TrainingArguments, batch_size: int, stage: int) -> None:
+        pass
 
-        return DataCollatorForSeq2Seq(tokenizer, max_length=max_input_length, padding=True, pad_to_multiple_of=8)
+    def update_data_collator(self, data_collator: Callable, stage: int) -> None:
+        pass
 
-    def get_compute_metrics(self, tokenizer: PreTrainedTokenizerBase) -> Callable:
-        return get_flores_compute_metrics(tokenizer)
-
-    def get_tokenized_dataset(self, tokenizer: PreTrainedTokenizerBase, training_arguments: TrainingArguments) -> DatasetDict:
-        return map(lambda c: c()(training_arguments), self.DATA_PROCESSOR_CLASSES)
-
-    def get_tokenized_datasets(self, tokenizer: PreTrainedTokenizerBase, training_arguments: TrainingArguments) -> DatasetDict:
-        return list(map(lambda c: c()(training_arguments), self.DATA_PROCESSOR_CLASSES))
-
+    def update_model(self, model: PreTrainedModel, stage: int) -> None:
+        pass
 
--- lme/experiments/flores_large_model/scaffold.py
+++ lme/experiments/flores_large_model/scaffold.py
@@ -2,8 +2,8 @@ from lme.data_processors.flores200 import (
     ScaffoldingInputMix3DataProcessor,
 )
 
-from lme.experiments.flores_large_model.baseline_20mil import FloresBaseline1B20milExperiment
+from lme.experiments.flores_large_model.baseline_20mil import FloresBaseline1BExperiment
 
 
-class FloresScaffoldInputMix31BExperiment(FloresBaseline1B20milExperiment):
+class FloresScaffoldInputMix31BExperiment(FloresBaseline1BExperiment):
     DATA_PROCESSOR_CLASSES = [ScaffoldingInputMix3DataProcessor]
