71c401ceff64477fff69b73034925140a7d95788
hlillemark <hlillemark@gmail.com>
2023-04-02 16:03:50 -0700

    Fix dataset processing

--- lme/training_dataset_utils/flores/scaffold_input_mix3_large_mt5.py
+++ lme/training_dataset_utils/flores/scaffold_input_mix3_large_mt5.py
@@ -20,6 +20,7 @@ RATIO_DATASET1 = 0.8  # 80% scaffold, 20% translation
 TOTAL_EXAMPLES = 20480000
 SEED = 42
 DATASET_NAME = "flores200_eng_input_scaffolding_mix3_large_mt5"
+BASE_DATASET_NAME = "flores200_eng_input_scaffolding_large_mt5"
 
 
 def main():
@@ -29,6 +30,14 @@ def main():
     })
 
     scaffold_dataset = tokenize_eng_scaffold_mt5(scaffold_dataset, MAX_SEQ_LEN, is_scaffold_input=True)
+
+    scaffold_dataset = DatasetDict({
+        "train": scaffold_dataset["train"],
+        "val": load_dataset("bri25yu/flores200_baseline_medium_mt5", split="val"),
+        "test": load_dataset("bri25yu/flores200_baseline_medium_mt5", split="test")
+        })
+
+    scaffold_dataset.push_to_hub(BASE_DATASET_NAME)
 
     baseline_dataset = load_dataset("bri25yu/flores200_baseline_medium_mt5")["train"]
 
