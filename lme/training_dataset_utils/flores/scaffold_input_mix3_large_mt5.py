52bac85604cdf5f9f17c06e88c3039ec666cbc30
hlillemark <hlillemark@gmail.com>
2023-04-02 19:38:20 -0700

    fix error in mix input dataset creation

--- lme/training_dataset_utils/flores/scaffold_input_mix3_large_mt5.py
+++ lme/training_dataset_utils/flores/scaffold_input_mix3_large_mt5.py
@@ -24,21 +24,7 @@ BASE_DATASET_NAME = "flores200_eng_input_scaffolding_large_mt5"
 
 
 def main():
-    train_dataset = load_dataset("hlillemark/flores200_eng_scaffolding_large")["train"]
-    scaffold_dataset = DatasetDict({
-        "train": train_dataset
-    })
-
-    scaffold_dataset = tokenize_eng_scaffold_mt5(scaffold_dataset, MAX_SEQ_LEN, is_scaffold_input=True)
-
-    scaffold_dataset = DatasetDict({
-        "train": scaffold_dataset["train"],
-        "val": load_dataset("bri25yu/flores200_baseline_medium_mt5", split="val"),
-        "test": load_dataset("bri25yu/flores200_baseline_medium_mt5", split="test")
-        })
-
-    scaffold_dataset.push_to_hub(BASE_DATASET_NAME)
-
+    scaffold_dataset = load_dataset(BASE_DATASET_NAME)
     baseline_dataset = load_dataset("bri25yu/flores200_baseline_medium_mt5")["train"]
 
     mixed_dataset = create_mix(scaffold_dataset, baseline_dataset, TOTAL_EXAMPLES, RATIO_DATASET1, seed=SEED)
