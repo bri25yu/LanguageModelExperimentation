5ff184823e1710cd10fb1988d85653aaaef8a45e
bri25yu <bri25yu@gmail.com>
2023-04-07 16:37:44 -0700

    adding packed 1b exp

---
+++ lme/experiments/flores_large_model/packed.py
@@ -0,0 +1,10 @@
+from lme.data_processors.flores200 import Packed2DataProcessor
+
+from lme.experiments.flores_large_model.baseline_20mil import FloresBaseline1BExperiment
+
+
+class FloresScaffoldInputMix31BExperiment(FloresBaseline1BExperiment):
+    # (2048 / 2) = 1024 // (2 ** 11 / 2 ** 1) = 2 ** 10
+    TARGET_TOTAL_BATCH_SIZE_PER_UPDATE = 2 ** 10
+
+    DATA_PROCESSOR_CLASSES = [Packed2DataProcessor]
