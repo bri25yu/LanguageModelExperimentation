f5b25540c3b3515f19f537470a875e49de3d1836
bri25yu <bri25yu@gmail.com>
2023-04-07 16:58:08 -0700

    cleaning up baseline 20mil file

--- lme/experiments/flores_large_model/baseline_20mil.py
+++ lme/experiments/flores_large_model/baseline_20mil.py
@@ -1,20 +1,12 @@
-from typing import Callable, List, Union
+from typing import Callable
 
-from datasets import DatasetDict
-
-from transformers import DataCollatorForSeq2Seq, Seq2SeqTrainer, TrainingArguments
-from transformers.tokenization_utils import PreTrainedTokenizerBase
+from transformers import TrainingArguments
 from transformers.modeling_utils import PreTrainedModel
 
-from lme.compute_metrics_utils.flores200 import get_flores_compute_metrics
-
-from lme.data_processors.abstract import AbstractDataProcessor
 from lme.data_processors.flores200 import BaselineMediumDataProcessor
-from lme.experiments.flores_600m_exps.baseline import FloresStagedExperimentBase
-
 from lme.model_mixins import MT51BModelMixin
-from lme.training_pipelines import FinetuneExperimentBase
-from lme.training_argument_mixins import FloresMT5FinetuneArgsMixin
+
+from lme.experiments.flores_600m_exps.baseline import FloresStagedExperimentBase
 
 
 class FloresBaseline1BExperiment(MT51BModelMixin, FloresStagedExperimentBase):
@@ -28,4 +20,3 @@ class FloresBaseline1BExperiment(MT51BModelMixin, FloresStagedExperimentBase):
 
     def update_model(self, model: PreTrainedModel, stage: int) -> None:
         pass
-
