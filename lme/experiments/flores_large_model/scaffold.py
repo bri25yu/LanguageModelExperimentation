b282ab454289d9073df29acf4e1e0fe66e95a931
bri25yu <bri25yu@gmail.com>
2023-04-08 22:16:03 -0700

    fixing scaffold input mix batch size

--- lme/experiments/flores_large_model/scaffold.py
+++ lme/experiments/flores_large_model/scaffold.py
@@ -6,4 +6,7 @@ from lme.experiments.flores_large_model.baseline_20mil import FloresBaseline1BEx
 
 
 class FloresScaffoldInputMix31BExperiment(FloresBaseline1BExperiment):
+    # (2048 / 2) = 1024 // (2 ** 11 / 2 ** 1) = 2 ** 10
+    TARGET_TOTAL_BATCH_SIZE_PER_UPDATE = 2 ** 10
+
     DATA_PROCESSOR_CLASSES = [ScaffoldingInputMix3DataProcessor]
