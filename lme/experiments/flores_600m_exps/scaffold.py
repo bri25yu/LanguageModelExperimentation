7ca0bd6affffeb9cd2a5990b4dbe9a228dec9070
hlillemark <hlillemark@gmail.com>
2023-04-11 00:04:26 -0700

    Add cotr 600m exp

--- lme/experiments/flores_600m_exps/scaffold.py
+++ lme/experiments/flores_600m_exps/scaffold.py
@@ -1,8 +1,15 @@
+from typing import Callable
+
+from transformers.tokenization_utils import PreTrainedTokenizerBase
+
 from lme.data_processors.flores200 import (
     ScaffoldingInputMixDataProcessor,
     ScaffoldingInputMix3DataProcessor,
-    ScaffoldingInputMix3LargeDataProcessor
+    ScaffoldingInputMix3LargeDataProcessor,
+    ScaffoldingOutputCOTRDataProcessor
 )
+
+from lme.compute_metrics_utils.flores200 import get_flores_compute_metrics_cotr
 
 from lme.training_argument_mixins.flores import FloresMT5FinetuneLargeArgsMixin
 
@@ -19,3 +26,10 @@ class FloresScaffoldInputMix3600MExperiment(FloresPacked600MExperiment):
 
 class FLoresScaffoldInputMix3Large600MExperiment(FloresMT5FinetuneLargeArgsMixin, FloresPacked600MExperiment):
     DATA_PROCESSOR_CLASSES = [ScaffoldingInputMix3LargeDataProcessor]
+
+
+class FloresScaffoldOutputCOTR600MExperiment(FloresScaffoldInputMix600MExperiment):
+    DATA_PROCESSOR_CLS = [ScaffoldingOutputCOTRDataProcessor]
+    
+    def get_compute_metrics(self, tokenizer: PreTrainedTokenizerBase) -> Callable:
+        return get_flores_compute_metrics_cotr(tokenizer)
