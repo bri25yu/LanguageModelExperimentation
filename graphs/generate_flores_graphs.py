98f31c93ee025d92aa402ff2d28c4ad6084af58b
bri25yu <bri25yu@gmail.com>
2023-05-02 21:16:54 -0700

    cleaning incomplete results; adding flores 1b and 3b graphs

--- graphs/generate_flores_graphs.py
+++ graphs/generate_flores_graphs.py
@@ -1,12 +1,6 @@
 from scripts.create_graph import *
 
 
-"""
-This is currently in progress, with missing results for 1B and 3B models. 
-
-Flores200 graphs are found at
-https://github.com/bri25yu/LanguageModelExperimentation/tree/d29d25047e86a2ef73dfbcd0f2ef8b0757a6df18
-"""
 plot_comparative_experiment(
     ["FloresBaseline600MExperiment", "FloresScaffoldInputMix3600MExperiment", "FloresPackedMix600MExperiment"],
     ["mT5 600M baseline", "mT5 600M English scaffold", "mT5 600M packed in context"],
@@ -14,4 +8,22 @@ plot_comparative_experiment(
     "eval/chrf++",
     "Validation set chrF++ score",
     "flores200_600m",
+)
+
+plot_comparative_experiment(
+    ["FloresBaseline1BExperiment", "FloresScaffoldInputMix31BExperiment", "FloresPacked1BExperiment"],
+    ["mT5 1B baseline", "mT5 1B English scaffold", "mT5 1B packed in context"],
+    "",
+    "eval/chrf++",
+    "Validation set chrF++ score",
+    "flores200_1b",
+)
+
+plot_comparative_experiment(
+    ["FloresBaseline3BExperiment", "FloresScaffold3BExperiment", "FloresPacked3BExperiment"],
+    ["mT5 3B baseline", "mT5 3B English scaffold", "mT5 3B packed in context"],
+    "",
+    "eval/chrf++",
+    "Validation set chrF++ score",
+    "flores200_3b",
 )
