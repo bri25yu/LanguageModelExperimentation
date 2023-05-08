490f120bf8fee5b140705b33a032ac3dd38fa38b
bri25yu <bri25yu@gmail.com>
2023-05-07 17:48:28 -0700

    adding tib2eng nllb results

--- graphs/generate_tib2eng_graphs.py
+++ graphs/generate_tib2eng_graphs.py
@@ -8,7 +8,7 @@ https://github.com/bri25yu/LanguageModelExperimentation/tree/07a9b30e05edaf419fb
 plot_comparative_experiment(
     ["TranslationNLLB600MExperiment", "TranslationMT5600MExperiment", "TranslationIncomplete4Experiment"],
     ["NLLB 600M baseline", "mT5 600M baseline", "mT5 600M completing an input"],
-    "",
+    "Classical Tibetan to English performance comparing NLLB and mT5 600M experimental configurations",
     "eval/bleu_score",
     "Validation set BLEU score",
     "tib2eng_600m",
@@ -17,7 +17,7 @@ plot_comparative_experiment(
 plot_comparative_experiment(
     ["TranslationNLLB1BExperiment", "TranslationMT51BExperiment", "TranslationIncompleteMT51BExperiment"],
     ["NLLB 1B baseline", "mT5 1B baseline", "mT5 1B completing an input"],
-    "",
+    "Classical Tibetan to English performance comparing NLLB and mT5 1B experimental configurations",
     "eval/bleu_score",
     "Validation set BLEU score",
     "tib2eng_1b",
@@ -26,7 +26,7 @@ plot_comparative_experiment(
 plot_comparative_experiment(
     ["TranslationNLLB3BExperiment", "TranslationMT53BExperiment", "TranslationIncompleteMT53BExperiment"],
     ["NLLB 3B baseline", "mT5 3B baseline", "mT5 3B completing an input"],
-    "",
+    "Classical Tibetan to English performance comparing NLLB and mT5 3B experimental configurations",
     "eval/bleu_score",
     "Validation set BLEU score",
     "tib2eng_3b",
@@ -35,7 +35,7 @@ plot_comparative_experiment(
 plot_comparative_experiment(
     ["TranslationMT5600MExperiment", "TranslationIncomplete4Experiment"],
     ["mT5 600M baseline", "mT5 600M completing an input"],
-    "Classical Tibetan to English performance",
+    "Classical Tibetan to English performance for mT5 600M experimental configurations",
     "eval/bleu_score",
     "Validation set BLEU score",
     "tib2eng_mt5_600m",
@@ -44,7 +44,7 @@ plot_comparative_experiment(
 plot_comparative_experiment(
     ["TranslationMT51BExperiment", "TranslationIncompleteMT51BExperiment"],
     ["mT5 1B baseline", "mT5 1B completing an input"],
-    "",
+    "Classical Tibetan to English performance for mT5 1B experimental configurations",
     "eval/bleu_score",
     "Validation set BLEU score",
     "tib2eng_mt5_1b",
@@ -53,7 +53,7 @@ plot_comparative_experiment(
 plot_comparative_experiment(
     ["TranslationMT53BExperiment", "TranslationIncompleteMT53BExperiment"],
     ["mT5 3B baseline", "mT5 3B completing an input"],
-    "",
+    "Classical Tibetan to English performance for mT5 3B experimental configurations",
     "eval/bleu_score",
     "Validation set BLEU score",
     "tib2eng_mt5_3b",
