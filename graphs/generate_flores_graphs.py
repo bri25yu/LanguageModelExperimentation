75392cd5eb8da7bd0db7e38fd8ff935a49f09323
bri25yu <bri25yu@gmail.com>
2023-05-07 21:12:06 -0700

    improving flores graphs

--- graphs/generate_flores_graphs.py
+++ graphs/generate_flores_graphs.py
@@ -2,9 +2,9 @@ from scripts.create_graph import *
 
 
 plot_comparative_experiment(
-    ["FloresBaseline600MExperiment", "FloresScaffoldInputMix3600MExperiment", "FloresPackedMix600MExperiment"],
+    ["FloresBaseline600MExperiment", "FloresScaffoldInputMix3600MExperiment", "FloresPacked600MExperiment"],
     ["mT5 600M baseline", "mT5 600M English scaffold", "mT5 600M packed in context"],
-    "",
+    "Flores200 performance\nfor mT5 600M experimental configurations",
     "eval/chrf++",
     "Validation set chrF++ score",
     "flores200_600m",
@@ -13,7 +13,7 @@ plot_comparative_experiment(
 plot_comparative_experiment(
     ["FloresBaseline1BExperiment", "FloresScaffoldInputMix31BExperiment", "FloresPacked1BExperiment"],
     ["mT5 1B baseline", "mT5 1B English scaffold", "mT5 1B packed in context"],
-    "",
+    "Flores200 performance\nfor mT5 1B experimental configurations",
     "eval/chrf++",
     "Validation set chrF++ score",
     "flores200_1b",
@@ -22,9 +22,9 @@ plot_comparative_experiment(
 plot_comparative_experiment(
     ["FloresBaseline3BExperiment", "FloresScaffold3BExperiment", "FloresPacked3BExperiment"],
     ["mT5 3B baseline", "mT5 3B English scaffold", "mT5 3B packed in context"],
-    "",
+    "Flores200 performance\nfor mT5 3B experimental configurations",
     "eval/chrf++",
     "Validation set chrF++ score",
     "flores200_3b",
-    y_min=5.0,
+    y_min=5,
 )
