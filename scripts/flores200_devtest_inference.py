f069cd14e8288d4ea2a117646b0edd9f478c9e63
hlillemark <hlillemark@gmail.com>
2023-05-15 23:32:35 -0700

    Create test eval for a18

--- scripts/flores200_devtest_inference.py
+++ scripts/flores200_devtest_inference.py
@@ -63,7 +63,7 @@ def run_eval(model_name: str, model_path_prefix: str, batch_size: int, n_example
 
 def get_indices(total_n: int, seed: int=42) -> List[int]:
     # Ensure reproducibility
-    set_numpy_seed(42)
+    set_numpy_seed(seed)
 
     possible_indices = list(range(total_n))
     select_indices = choice(possible_indices, size=(n_examples,), replace=False)
@@ -80,9 +80,9 @@ def run_eval_subsample(model_name: str, model_path_prefix: str, batch_size: int,
 
 if __name__ == "__main__":
     model_path_prefix = "hlillemark"
-    n_examples = 1_000_000
+    n_examples = 10_000
     bs_600m = 32
 
-    run_eval_subsample("mt5-600M-flores200-scaffold", model_path_prefix, bs_600m, n_examples)
-    run_eval_subsample("mt5-600M-flores200-baseline", model_path_prefix, bs_600m, n_examples)
-    run_eval_subsample("mt5-600M-flores200-packed", model_path_prefix, bs_600m, n_examples)
+    run_eval_subsample("mt5-1B-flores200-scaffold", model_path_prefix, bs_600m, n_examples)
+    # run_eval_subsample("mt5-600M-flores200-baseline", model_path_prefix, bs_600m, n_examples)
+    # run_eval_subsample("mt5-600M-flores200-packed", model_path_prefix, bs_600m, n_examples)
