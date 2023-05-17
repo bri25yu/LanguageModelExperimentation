b018c21d2a94ae7b780dc9d0f54cb91a43cf8175
hlillemark <hlillemark@gmail.com>
2023-05-17 13:45:09 -0700

    Make agg script more modular

--- statistics/agg_avg_table.py
+++ statistics/agg_avg_table.py
@@ -4,10 +4,12 @@ from IPython.display import display
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-INPUT_DATASETS = ['flores200_devtest_mt5-600m-flores200-baseline', 'flores200_devtest_mt5-600m-flores200-scaffold', 'flores200_devtest_mt5-600m-flores200-packed']
-
+INPUT_DATASETS = [] 
+PARAM_COUNT = '600m'
 loaded = []
 names = ['baseline', 'scaffold', 'packed']
+for name in names:
+    INPUT_DATASETS.append('flores200_devtest_mt5-{}-flores200-{}'.format(PARAM_COUNT, name))
 
 for INPUT_DATASET, name in zip(INPUT_DATASETS, names):
     p = pd.read_csv(os.path.join(BASE_DIR, INPUT_DATASET, 'avg_stats.csv'))
@@ -21,4 +23,4 @@ for i in range(1, len(loaded)):
 
 # display and save
 display(merged)
-pd.DataFrame.to_csv(merged, os.path.join(BASE_DIR, 'agg_avg_table.csv'), index=False)
+pd.DataFrame.to_csv(merged, os.path.join(BASE_DIR, f'agg_avg_table_{PARAM_COUNT}.csv'), index=False)
