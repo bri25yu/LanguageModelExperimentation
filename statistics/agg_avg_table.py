55f5be06a210fbcd34dc81aedf6bbc809216a25d
hlillemark <hlillemark@gmail.com>
2023-05-23 11:11:52 -0700

    Update agg avg table script + results with eng-xx and xx-eng

--- statistics/agg_avg_table.py
+++ statistics/agg_avg_table.py
@@ -13,6 +13,14 @@ for name in names:
 
 for INPUT_DATASET, name in zip(INPUT_DATASETS, names):
     p = pd.read_csv(os.path.join(BASE_DIR, INPUT_DATASET, 'avg_stats.csv'))
+    p = p[p['label'] != 'xx-yy']
+    # Load lang_to_xx and xx_to_lang
+    lang_to_xx = pd.read_csv(os.path.join(BASE_DIR, INPUT_DATASET, 'lang_to_xx_mt5_pretrain.csv'))
+    xx_to_lang = pd.read_csv(os.path.join(BASE_DIR, INPUT_DATASET, 'xx_to_lang_mt5_pretrain.csv'))
+    # Add row for eng-xx and xx-eng
+    p = pd.concat([pd.DataFrame({'label': 'eng-xx', 'chrF++': lang_to_xx[lang_to_xx['label'] == 'eng_Latn-xx']['chrF++']}), p])
+    p = pd.concat([pd.DataFrame({'label': 'xx-eng', 'chrF++': xx_to_lang[xx_to_lang['label'] == 'xx-eng_Latn']['chrF++']}), p])
+    p['chrF++'] = p['chrF++'].round(1)
     p.rename(columns={'chrF++': 'chrF++_{}'.format(name)}, inplace=True)
     loaded.append(p)
 
