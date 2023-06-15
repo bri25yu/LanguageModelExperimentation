139f30d9b8865c00cb6af5e5513147c74a04c5d4
bri25yu <bri25yu@gmail.com>
2023-06-15 12:55:49 -0700

    consolidating plot pretrain graph

--- statistics/plot_pretrain.py
+++ statistics/plot_pretrain.py
@@ -1,24 +1,37 @@
-import math
-import matplotlib.pyplot as plt
-import os 
-import pandas as pd
+from os.path import abspath, dirname, join
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+from matplotlib.pyplot import subplots
+from matplotlib import rcParams
+rcParams.update({"font.size": 24})
 
-PARAM_COUNT = '600m'
-loaded = []
-names = ['baseline', 'scaffold', 'packed']
-PLOT_TYPE = 'lang_to_xx'
+from pandas import read_csv
 
-fig, ax = plt.subplots()
 
-for name in names:
-    p = pd.read_csv(os.path.join(BASE_DIR, PARAM_COUNT, name, 'pretraining', f'{PLOT_TYPE}_mt5_pretrain.csv'))
+BASE_DIR = dirname(abspath(__file__))
+save_path = join(BASE_DIR, "flores_pretrain_size.pdf")
 
-    ax.scatter(x=p['size'].astype(int).apply(lambda x: math.log10(x)), y=p['chrF++'])
-    ax.set_xlabel("Pretraining data size (log_10)")
-    ax.set_ylabel("chrF++")
-    ax.set_title(f"mT5 {PARAM_COUNT} Pretraining data size vs post-finetuning chrF++ score, {PLOT_TYPE}")
 
-ax.legend(names)
-plt.show()
+plot_types = ["xx_to_lang", "lang_to_xx"]
+param_counts = ["600m", "1b", "3b"]
+names = ["baseline", "scaffold", "packed"]
+
+
+rows, cols = 2, 3
+fig, axs = subplots(rows, cols, figsize=(10 * cols, 8 * rows), sharex=True, sharey=True)
+
+for plot_type, plot_type_axs in zip(plot_types, axs):
+    for param_count, param_count_ax in zip(param_counts, plot_type_axs):
+        for name in names:
+            df = read_csv(join(BASE_DIR, param_count, name, "pretraining", f"{plot_type}_mt5_pretrain.csv"))
+            param_count_ax.scatter(df["size"], df["chrF++"], label=name)
+
+        param_count_ax.set_xscale("log")
+        param_count_ax.set_title(f"mT5 {param_count}, {plot_type} condition")
+        param_count_ax.legend()
+
+fig.suptitle("Pretrain dataset size vs chrF++ score")
+fig.supxlabel("Pretrain dataset # examples")
+fig.supylabel("chrF++ score")
+
+fig.tight_layout()
+fig.savefig(save_path, format="pdf")
