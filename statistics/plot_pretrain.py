8377c10f4caa941bd24071f52b21dc0ac0b4ad24
bri25yu <bri25yu@gmail.com>
2023-06-22 09:58:10 -0700

    improving pretrain graph

--- statistics/plot_pretrain.py
+++ statistics/plot_pretrain.py
@@ -11,7 +11,8 @@ BASE_DIR = dirname(abspath(__file__))
 save_path = join(BASE_DIR, "flores_pretrain_size.pdf")
 
 
-plot_types = ["xx_to_lang", "lang_to_xx"]
+plot_types = ["lang_to_xx", "xx_to_lang"]
+plot_type_titles = ["from", "into"]
 param_counts = ["600m", "1b", "3b"]
 names = ["baseline", "scaffold", "packed"]
 
@@ -19,14 +20,14 @@ names = ["baseline", "scaffold", "packed"]
 rows, cols = 2, 3
 fig, axs = subplots(rows, cols, figsize=(10 * cols, 8 * rows), sharex=True, sharey=True)
 
-for plot_type, plot_type_axs in zip(plot_types, axs):
+for plot_type, plot_type_title, plot_type_axs in zip(plot_types, plot_type_titles, axs):
     for param_count, param_count_ax in zip(param_counts, plot_type_axs):
         for name in names:
             df = read_csv(join(BASE_DIR, param_count, name, "pretraining", f"{plot_type}_mt5_pretrain.csv"))
             param_count_ax.scatter(df["size"], df["chrF++"], label=name)
 
         param_count_ax.set_xscale("log")
-        param_count_ax.set_title(f"mT5 {param_count}, {plot_type} condition")
+        param_count_ax.set_title(f"mT5 {param_count}, translation {plot_type_title}")
         param_count_ax.legend()
 
 fig.suptitle("Pretrain dataset size vs chrF++ score")
