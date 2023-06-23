8a5f5af01545aaceefadf38791073a88d88d019a
bri25yu <bri25yu@gmail.com>
2023-06-23 14:36:48 -0700

    updating graphs

--- graphs/generate_flores_graphs.py
+++ graphs/generate_flores_graphs.py
@@ -4,22 +4,22 @@ from graphs.create_graph import *
 plot_comparative_series(
     batch_graph_kwargs=[
         {
-            "experiment_names": ["FloresBaseline600MExperiment", "FloresScaffoldInputMix3600MExperiment", "FloresPacked600MExperiment"],
-            "legend_labels": ["mT5 600M baseline", "mT5 600M English scaffold", "mT5 600M packed in context"],
+            "experiment_names": ["FloresBaseline600MExperiment", "FloresScaffold600MExperiment", "FloresPacked600MExperiment"],
+            "legend_labels": ["mT5 600M baseline", "mT5 600M ParSE", "mT5 600M MiPS"],
             "title": "mT5 600M",
         },
         {
-            "experiment_names": ["FloresBaseline1BExperiment", "FloresScaffoldInputMix31BExperiment", "FloresPacked1BExperiment"],
-            "legend_labels": ["mT5 1B baseline", "mT5 1B English scaffold", "mT5 1B packed in context"],
+            "experiment_names": ["FloresBaseline1BExperiment", "FloresScaffold1BExperiment", "FloresPacked1BExperiment"],
+            "legend_labels": ["mT5 1B baseline", "mT5 1B ParSE", "mT5 1B MiPS"],
             "title": "mT5 1B",
         },
         {
             "experiment_names": ["FloresBaseline3BExperiment", "FloresScaffold3BExperiment", "FloresPacked3BExperiment"],
-            "legend_labels": ["mT5 3B baseline", "mT5 3B English scaffold", "mT5 3B packed in context"],
+            "legend_labels": ["mT5 3B baseline", "mT5 3B ParSE", "mT5 3B MiPS"],
             "title": "mT5 3B",
         },
     ],
-    title="Flores200 performance for mT5 experimental configurations",
+    title="Flores200 performance for mT5 baseline, ParSE, and MiPS configurations",
     property_name="eval/chrf++",
     y_label="Validation set chrF++ score",
     save_name="flores200",
--- graphs/generate_tib2eng_graphs.py
+++ graphs/generate_tib2eng_graphs.py
@@ -5,48 +5,23 @@ plot_comparative_series(
     batch_graph_kwargs=[
         {
             "experiment_names": ["TranslationMT5600MExperiment", "TranslationIncomplete4Experiment", "TranslationNLLB600MExperiment"],
-            "legend_labels": ["mT5 600M baseline", "mT5 600M completing an input", "NLLB 600M baseline"],
+            "legend_labels": ["mT5 600M baseline", "mT5 600M POSE", "NLLB 600M"],
             "title": "mT5 vs NLLB 600M",
         },
         {
             "experiment_names": ["TranslationMT51BExperiment", "TranslationIncompleteMT51BExperiment", "TranslationNLLB1BExperiment"],
-            "legend_labels": ["mT5 1B baseline", "mT5 1B completing an input", "NLLB 1B baseline"],
+            "legend_labels": ["mT5 1B baseline", "mT5 1B POSE", "NLLB 1B"],
             "title": "mT5 vs NLLB 1B",
         },
         {
             "experiment_names": ["TranslationMT53BExperiment", "TranslationIncompleteMT53BExperiment", "TranslationNLLB3BExperiment"],
-            "legend_labels": ["mT5 3B baseline", "mT5 3B completing an input", "NLLB 3B baseline"],
+            "legend_labels": ["mT5 3B baseline", "mT5 3B POSE", "NLLB 3B"],
             "title": "mT5 vs NLLB 3B",
         },
     ],
-    title="Comparing mT5 and NLLB on Classical Tibetan to English",
+    title="Comparing mT5 baseline, mT5 POSE, and NLLB on Classical Tibetan to English",
     property_name="eval/bleu_score",
     y_label="Validation set BLEU score",
     save_name="tib2eng",
     y_lim=[0, 38],
-)
-
-plot_comparative_series(
-    batch_graph_kwargs=[
-        {
-            "experiment_names": ["TranslationMT5600MExperiment", "TranslationIncomplete4Experiment"],
-            "legend_labels": ["mT5 600M baseline", "mT5 600M completing an input"],
-            "title": "mT5 600M",
-        },
-        {
-            "experiment_names": ["TranslationMT51BExperiment", "TranslationIncompleteMT51BExperiment"],
-            "legend_labels": ["mT5 1B baseline", "mT5 1B completing an input"],
-            "title": "mT5 1B",
-        },
-        {
-            "experiment_names": ["TranslationMT53BExperiment", "TranslationIncompleteMT53BExperiment"],
-            "legend_labels": ["mT5 3B baseline", "mT5 3B completing an input"],
-            "title": "mT5 3B",
-        },
-    ],
-    title="Classical Tibetan to English performance for mT5 experimental configurations",
-    property_name="eval/bleu_score",
-    y_label="Validation set BLEU score",
-    save_name="tib2eng_mt5",
-    y_lim=[0, 34],
 )
