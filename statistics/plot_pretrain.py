f508ecebfcf9bf46ef50250eb03abe5d53471ba1
bri25yu <bri25yu@gmail.com>
2023-06-24 02:10:43 -0700

    update graph

--- statistics/plot_pretrain.py
+++ statistics/plot_pretrain.py
@@ -15,6 +15,7 @@ plot_types = ["lang_to_xx", "xx_to_lang"]
 plot_type_titles = ["from", "into"]
 param_counts = ["600m", "1b", "3b"]
 names = ["baseline", "scaffold", "packed"]
+labels = ["baseline", "ParSE", "MiPS"]
 
 
 rows, cols = 2, 3
@@ -22,9 +23,9 @@ fig, axs = subplots(rows, cols, figsize=(10 * cols, 8 * rows), sharex=True, shar
 
 for plot_type, plot_type_title, plot_type_axs in zip(plot_types, plot_type_titles, axs):
     for param_count, param_count_ax in zip(param_counts, plot_type_axs):
-        for name in names:
+        for name, label in zip(names, labels):
             df = read_csv(join(BASE_DIR, param_count, name, "pretraining", f"{plot_type}_mt5_pretrain.csv"))
-            param_count_ax.scatter(df["size"], df["chrF++"], label=name)
+            param_count_ax.scatter(df["size"], df["chrF++"], label=label)
 
         param_count_ax.set_xscale("log")
         param_count_ax.set_title(f"mT5 {param_count}, translation {plot_type_title}")
